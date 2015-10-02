b2c27f5b72d41137501683354b924b57441bb682
Kai Curry <kai@sundaysenergy.com>
2015-10-01 23:01:45 -0500

    fix description data

--- devServer2.js
+++ devServer2.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import Express from 'express';
-
+import _ from 'lodash';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
@@ -9,11 +9,12 @@ import webpackConfig from './webpack.config.dev';
 import Wreck from 'wreck';
 import {camelizeKeys} from 'humps';
 
-let apiData = null;
+// import { App } from './src/App';
 
-var app = Express();
-var port = 3000;
-var compiler = webpack(webpackConfig);
+const app = Express();
+const port = 3000;
+
+const compiler = webpack(webpackConfig);
 
 app.use(webpackDevMiddleware(compiler, {
   noInfo: true,
@@ -24,22 +25,47 @@ app.use(webpackHotMiddleware(compiler));
 
 app.use(Express.static('public'));
 
-app.get('/api', function(req, res) {
+let apiData = null;
+
+function fixDataItem(item) {
+  item.presentations = item.presentations.map((presentation) => {
+    const description = {};
+    _.each(presentation.description, desc =>
+      description[desc.fieldLabel.toLowerCase()] = desc.fieldValue
+    )
+    presentation.description = camelizeKeys(description);
+    return presentation;
+  })
+  return item;
+}
+
+function fetchData(cb) {
   var fullUrl = 'http://www.xcdsystem.com/icfp/admin/program.json';
   if (apiData) {
     console.log('return cached data');
-    res.send(apiData);
+    cb(apiData);
   }
   else {
     console.log('fetch new data');
     Wreck.get(fullUrl, {json: true}, (err, response, payload) => {
       console.log('transform new data');
-      apiData = camelizeKeys(payload);
+      apiData = _.map(camelizeKeys(payload), fixDataItem);
       console.log('return new data');
-      res.send(apiData);
+      cb(apiData);
     })
   }
+}
+fetchData((data) => {return});
+app.get('/api', function(req, res) {
+  fetchData((data) => res.send(data));
 });
+
+// app.get('/render', function(req, res) {
+//   fetchData( items => {
+//     const html = React.renderToString(<App items={items} />);
+//     res.send(html);
+//   })
+// });
 
 app.listen(port, 'localhost', function (err) {
   if (err) {
