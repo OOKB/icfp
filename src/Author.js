3b0ccae9ae0a9b15b4d1492d10a32c647933e1d3
Kai Curry <kai@sundaysenergy.com>
2015-10-05 18:47:46 -0500

    co-author and presenter tags

--- src/Author.js
+++ src/Author.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 
+class Presenter extends Component {
+  render() {
+    const { children } = this.props;
+    return <presenter className="presenter">{ children }</presenter>
+  }
+}
+
+class CoAuthor extends Component {
+  render() {
+    const { children } = this.props;
+    return <author className="co-author">{ children }</author>
+  }
+}
+
 class Author extends Component {
   render() {
     const { firstname, lastname, company, presenter, ...rest } = this.props;
-    let className = 'co-author';
+
+    let AuthorTag = CoAuthor;
     if (presenter) {
-      className = 'presenter';
+      AuthorTag = Presenter;
     }
     const fullName = `${firstname} ${lastname}`;
 
   return (
-      <author className={className}>
+      <AuthorTag>
         <fullname>{fullName}</fullname>
         <company>{company}</company>
-      </author>
+      </AuthorTag>
     );
   }
 }
