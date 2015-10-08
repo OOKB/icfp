0e5a35e0769f13d0972daaa67aed2fe1b15d9480
Kristian Bjørnard <kristian.bjornard@gmail.com>
2015-10-07 21:37:34 -0400

    Try adding moderator.js

---
+++ src/Moderator.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Moderator extends Component {
+  render() {
+    const { firstname, lastname, company, ...rest } = this.props;
+
+    const fullName = `${firstname} ${lastname}`;
+    const separator = ', ';
+
+  return (
+      <moderator>
+        <fullname>{fullName}</fullname>
+        {separator}
+        <company>{company}</company>
+      </moderator>
+    );
+  }
+}
+
+export default Author;
