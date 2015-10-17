92e1883017ab06fa89fe9d52c6bb84826fdbc576
Kai Curry <kai@sundaysenergy.com>
2015-10-16 22:14:51 -0500

    route index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 
+import RouteIndex from './RouteIndex';
 import App from './App';
 import Apps from './Apps';
 import Authors from './Authors';
@@ -21,6 +22,7 @@ fetch(fullUrl)
     React.render(
       <Router>
         <Route path="/" component={App} items={'kai'}>
+          <IndexRoute component={RouteIndex} />
           <Route path="posters" component={Apps} items={json.posters} type="posters" />
           <Route path="sessions" component={Apps} items={json.sessions} type="sessions" />
           <Route path="other" component={Apps} items={json.sessions} type="other" />
